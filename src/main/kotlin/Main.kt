7ebccc69e9e0d8e2e54b242e97586ac18dde8145
Teo <mgttmich@gmail.com>
2023-12-16 16:58:19 +0200

    Basics Kotlin - Flatten

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,20 @@ fun main() {
     println(numbers.associateWith { it.length })
     println(numbers.associateBy { it.first().uppercase() })
     println(numbers.associateBy(keySelector = { it.first().uppercase() }, valueTransform = { it.length }))
+    println("\n")
+
+    //Flatten
+    val numbersSets = listOf(setOf(1, 2, 3), setOf(4, 5, 6), setOf(7, 8, 9))
+//    for (numbers in numbersSets) {
+//        for (number in numbers) {
+//            println(number)
+//        }
+//        println("\n")
+//    }
+
+    //Flatten - Two dimensional array -> One dimensional array
+    val numbersFlatten = numbersSets.flatten()
+    for (number in numbersFlatten) {
+        println(number)
+    }
 }
